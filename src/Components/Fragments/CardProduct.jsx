73ad7078a26c920c34d185500c59f610f65b0789
muazhri <muhaiminaziz216@gmail.com>
2025-03-19 22:32:12 +0700

    rendering list reactjs

--- src/Components/Fragments/CardProduct.jsx
+++ src/Components/Fragments/CardProduct.jsx
@@ -3,7 +3,7 @@ import Button from "../Elements/Button";
 const CardProduct = (props) => {
     const {children} = props
     return (
-        <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow">
+        <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow mx-2 flex flex-col justify-between">
         {children}        
        </div>
     )
@@ -11,14 +11,14 @@ const CardProduct = (props) => {
 }
 
 const Body = (props) => { 
-    const {children, title} = props ;  
+    const {children, name} = props ;  
     return  (
-        <div className="px-5 pb-5">
+        <div className="px-5 pb-5 h-full">
         <a href="">
-          <h5 className="text-xl font-semibold tracking-tight  dark:text-white">
-            {title}
+          <h5 className="text-xl font-semibold tracking-tight  dark:text-white ">
+            {name}
           </h5>
-          <p className="text-slate-300 mt-2 text-sm">
+          <p className="text-slate-300 mt-2 text-sm ">
             {children}
         </p>
         </a>
--- src/Pages/products.jsx
+++ src/Pages/products.jsx
@@ -1,17 +1,36 @@
 import CardProduct from "../Components/Fragments/CardProduct";
 
+const Products = [
+  {
+    id: 1,
+    image: "/public/image/shoes1.jpg",
+    name: "Sepatu baru",
+    harga: "Rp. 200.000",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magni quia error dolores architecto unde, excepturi vitae asperiores dolorum dicta!",
+  },
+  {
+    id: 2,
+    image: "/public/image/shoes2.jpg",
+    name: "Sepatu Biru",
+    harga: "Rp. 500.000",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magni quia error dolores architecto unde, excepturi vitae asperiores dolorum dicta!",
+  },  
+];
+
 const ProductPage = () => {
   return (
     <div className="flex items-center justify-center h-screen py-5">
-      <CardProduct>
-        <CardProduct.Image image="/public/image/shoes1.jpg"></CardProduct.Image>
-        <CardProduct.Body title="Sepatu baru">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
-          magni quia error dolores architecto unde, excepturi vitae asperiores
-          dolorum dicta!
-        </CardProduct.Body>
-        <CardProduct.Footer harga="Rp. 200.000"></CardProduct.Footer>
-      </CardProduct>
+      {Products.map((product) => (
+        <CardProduct>
+          <CardProduct.Image image={product.image}></CardProduct.Image>
+          <CardProduct.Body name={product.name}>
+            {product.description}
+          </CardProduct.Body>
+          <CardProduct.Footer harga={product.harga}></CardProduct.Footer>
+        </CardProduct>
+      ))}
     </div>
   );
 };
